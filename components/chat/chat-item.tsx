e2e3d26b4c82c925a82f3cfaccfddc0d063cac76
ilzarachman <ilzarachman2@gmail.com>
2024-09-09 12:50:52 +0700

    fix: link

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -7,78 +7,108 @@ import { Separator } from "@/components/ui/separator";
 import { ThumbsUp, MessageSquareDiff } from "lucide-react";
 import { Button } from "../ui/button";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import FeedbackForm from "@/components/feedback/feedback-form";
 
 export function UserChat({ text }: { text: string }) {
-    return (
-        <>
-            <div className="p-7 max-w-3xl flex gap-5 w-full min-w-full bg-muted rounded-3xl pr-14 ">
-                <Avatar>
-                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <p className="text-foreground pt-2 w-full">{text}</p>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="p-7 max-w-3xl flex gap-5 w-full min-w-full bg-muted rounded-3xl pr-14 ">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <p className="text-foreground pt-2 w-full">{text}</p>
+      </div>
+    </>
+  );
 }
 
-export function BotChat({ prompt, text, generation }: { prompt: string; text: string; generation: boolean }) {
-    const [isFeedbackOpen, setIsFeedbackOpen] = React.useState<boolean>(false);
+function LinkRenderer(props: any) {
+  console.log({ props });
+  return (
+    <a href={props.href} target="_blank" rel="noreferrer">
+      {props.children}
+    </a>
+  );
+}
 
-    function closeFeedback() {
-        setIsFeedbackOpen(false);
-    }
+export function BotChat({
+  prompt,
+  text,
+  generation,
+}: {
+  prompt: string;
+  text: string;
+  generation: boolean;
+}) {
+  const [isFeedbackOpen, setIsFeedbackOpen] = React.useState<boolean>(false);
 
-    return (
-        <>
-            <div className="p-4 my-4 mt-2 max-w-3xl flex gap-5 w-full min-w-full">
-                <Avatar>
-                    <AvatarFallback className="bg-transparent">
-                        <BotMessageSquare className="w-6 h-6" />
-                    </AvatarFallback>
-                </Avatar>
+  function closeFeedback() {
+    setIsFeedbackOpen(false);
+  }
 
-                {text != "" ? (
-                    <div className="flex flex-col gap-2">
-                        <div className="pt-2 g-markdown-container" data-generation={generation}>
-                            <Markdown remarkPlugins={[remarkGfm]}>
-                                {text}
-                            </Markdown>
-                        </div>
-                        {!generation ?
-                            <Dialog open={isFeedbackOpen} onOpenChange={setIsFeedbackOpen}>
-                                <DialogTrigger asChild>
-                                    <Button variant="outline" className="w-[150px]"><MessageSquareDiff className="w-5 h-5 mr-2" /> Feedback</Button>
-                                </DialogTrigger>
-                                <DialogContent>
-                                    <DialogHeader>
-                                        <DialogTitle>Give this dialog a feedback!</DialogTitle>
-                                        <DialogDescription>
-                                            Feedback will help us improve.
-                                        </DialogDescription>
-                                    </DialogHeader>
+  return (
+    <>
+      <div className="p-4 my-4 mt-2 max-w-3xl flex gap-5 w-full min-w-full">
+        <Avatar>
+          <AvatarFallback className="bg-transparent">
+            <BotMessageSquare className="w-6 h-6" />
+          </AvatarFallback>
+        </Avatar>
 
-                                    <FeedbackForm bot_answer={text} prompt={prompt} onClose={closeFeedback} />
-
-                                </DialogContent>
-                            </Dialog>
-                            : ""}
-                    </div>
-                ) : (
-                    <div className="flex flex-col w-full ">
-                        <div className="animate-pulse bg-muted h-3 w-full rounded-full mt-1"></div>
-                        <div className="animate-pulse bg-muted h-3 w-[80%] rounded-full mt-2"></div>
-                    </div>
-                )}
+        {text != "" ? (
+          <div className="flex flex-col gap-2">
+            <div
+              className="pt-2 g-markdown-container"
+              data-generation={generation}
+            >
+              <Markdown
+                remarkPlugins={[remarkGfm]}
+                components={{ a: LinkRenderer }}
+              >
+                {text}
+              </Markdown>
             </div>
-        </>
-    );
+            {!generation ? (
+              <Dialog open={isFeedbackOpen} onOpenChange={setIsFeedbackOpen}>
+                <DialogTrigger asChild>
+                  <Button variant="outline" className="w-[150px]">
+                    <MessageSquareDiff className="w-5 h-5 mr-2" /> Feedback
+                  </Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Give this dialog a feedback!</DialogTitle>
+                    <DialogDescription>
+                      Feedback will help us improve.
+                    </DialogDescription>
+                  </DialogHeader>
+
+                  <FeedbackForm
+                    bot_answer={text}
+                    prompt={prompt}
+                    onClose={closeFeedback}
+                  />
+                </DialogContent>
+              </Dialog>
+            ) : (
+              ""
+            )}
+          </div>
+        ) : (
+          <div className="flex flex-col w-full ">
+            <div className="animate-pulse bg-muted h-3 w-full rounded-full mt-1"></div>
+            <div className="animate-pulse bg-muted h-3 w-[80%] rounded-full mt-2"></div>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
